6be5aeb021bb43e01c3c9449292de84769d202ca
Cassio Trindade <cassio.trindade@pucrs.br>
2018-01-05 16:16:45 -0200

    alterado nome e senha do mongo

--- config.js
+++ config.js
@@ -6,7 +6,8 @@ const config = {
     base_url: process.env.BASE_URL || 'http://localhost:3021',
     db: {
        // uri: 'mongodb://localhost/animal',
-        uri: 'mongodb://animal:animal@localhost/animal',
+        uri: 'mongodb://root:G0W40bT2QE39@localhost/animal',
+       // uri: 'mongodb://animal:animal@localhost/animal',
     },
 }
 
